806c9ff383f3e664ac104ef24bea3e24819e1c64
marco <marco.antonio.rg340@gmail.com>
2026-03-07 17:10:49 -0300

    Started to implement Event Sourcing

---
+++ FinancesApp_CQRS/Dispatchers/EventDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using FinancesApp_CQRS.Interfaces;
+
+namespace FinancesApp_CQRS.Dispatchers;
+public class EventDispatcher
+{
+    private readonly Dictionary<Type, List<object>> _handlers = [];
+
+    public void Register<TEvent>(IEventHandler<TEvent> handler) where TEvent : IDomainEvent
+    {
+        var type = typeof(TEvent);
+        if (!_handlers.ContainsKey(type))
+            _handlers[type] = [];
+        _handlers[type].Add(handler);
+    }
+
+    public void Dispatch(IEnumerable<IDomainEvent> events)
+    {
+        foreach (var evt in events)
+        {
+            if (_handlers.TryGetValue(evt.GetType(), out var handlers))
+                foreach (var handler in handlers)
+                    ((dynamic)handler).Handle((dynamic)evt);
+        }
+    }
+}
---
+++ FinancesApp_CQRS/Interfaces/IEventHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace FinancesApp_CQRS.Interfaces;
+public interface IEventHandler<TEvent> where TEvent : IDomainEvent
+{
+    void Handle(TEvent evt);
+}
